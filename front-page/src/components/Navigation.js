52d36cc50726b7a5dc700d72dbe96b99c8f1559a
SamirTaous <hgta626@gmail.com>
2024-12-05 18:24:18 +0100

    Added front page directory

---
+++ front-page/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const Navigation = () => {
+  return (
+    <Button className="nav-button" variant="contained" color="primary" sx={{backgroundColor: 'black'}}>
+        Hello MUI
+    </Button>
+  )
+}
+
+export default Navigation
