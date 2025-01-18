8e9a615ffd278cd8ca33c6eee9cba8b4b3e2c9de
mouad4949 <mouadrguibi900@gmail.com>
2025-01-18 05:52:03 +0100

    api configured

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,7 +4,6 @@ const API_URL1 = import.meta.env.VITE_AUTH_API_URL; // API for authentification
 const API_URL2 = import.meta.env.VITE_TRANSACTION_API_URL; // API for transactions
 const API_URL3 = import.meta.env.VITE_LOAN_API_URL; // API for loan applications
 const API_URL4 = import.meta.env.VITE_ACCOUNT_API_URL; // API for accounts
-
 // API for login (assuming token is returned on login)
 export const login = async (username, password) => {
   try {
@@ -15,10 +14,10 @@ export const login = async (username, password) => {
   }
 };
 
-// Fetch transactions by client ID with token
-export const getTransactionsByClientId = async (clientId, token) => {
+// Fetch transactions by compte Id with token
+export const getTransactionsByCompteId = async (compteId, token) => {
   try {
-    const response = await axios.get(`${API_URL2}/operationsClient/${clientId}`, {
+    const response = await axios.get(`${API_URL2}/operations/${compteId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -28,6 +27,16 @@ export const getTransactionsByClientId = async (clientId, token) => {
     throw error.response ? error.response.data : error.message;
   }
 };
+
+// // Fetch transactions by client ID
+// export const getTransactionsByClientId = async (clientId) => {
+//     try {
+//       const response = await axios.get(`${API_URL2}/ConsulterOperation/${clientId}`);
+//       return response.data;
+//     } catch (error) {
+//       throw error.response ? error.response.data : error.message;
+//     }
+//   };
 
 // Create a new loan application
 export const createLoanApplication = async (loanData) => {
@@ -50,6 +59,7 @@ export const fetchLoanApplications = async (userId) => {
 };
 
 // Fetch accounts by client id 
+
 export const getAccountsbyClientID = async (clientId, token) => {
   try {
     const response = await axios.get(`${API_URL4}/client/${clientId}`, {
